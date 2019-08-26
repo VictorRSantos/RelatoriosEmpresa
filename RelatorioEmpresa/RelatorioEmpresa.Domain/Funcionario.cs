8a06e77a6855dd4eb75689b3aeea6aa363745347
Victor Rodrigues <victor.rsantos@outlook.com>
2019-08-25 22:50:38 -0300

    Segundo Commit

---
+++ RelatorioEmpresa/RelatorioEmpresa.Domain/Funcionario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelatorioEmpresa.Domain
+{
+    public class Funcionario
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Andar { get; set; }
+
+        public string Transporte { get; set; }
+
+        public DateTime Inicio { get; set; }
+
+        public DateTime Fim { get; set; }
+
+
+
+    }
+}
---
+++ RelatorioEmpresa/RelatorioEmpresa.UI.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RelatorioEmpresa.UI.Web.MvcApplication" Language="C#" %>
